9fd802f6ed5e9e0aa5d658f0845f225dbb2dd1d2
tamal15 <tamal15-13243@diu.edu.bd>
2022-09-06 00:11:56 +0600

    main part

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 // import logo from './logo.svg';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import './App.css';
-import Header from './Shared/Header/Header';
-import Home from './Pages/Home/Home';
-import BuyerDashboard from './Pages/Home/BuyerDashboard/BuyerDashboard';
+
+
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-      <Header></Header>
-      <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/buyer" element={<BuyerDashboard />} />
       
-      </Routes>
-      </BrowserRouter>
      
     </div>
   );
